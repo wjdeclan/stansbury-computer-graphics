2c762d2bd70814b106544660ea381d7d4ab66bf3
wjdeclan <wjdeclan@gmail.com>
2015-03-12 12:03:00 -0400

    Revert "Stahp"
    This reverts commit eec6599911eebd25bd39540dcbe5284c37cbcc3b.

--- ClientGame/build.lua
+++
@@ -1,7 +0,0 @@
--- This is a configuration file for the final build/package process. 
---  At the moment it just sets the name, but there may be more settings
---  in the future.
-
-game_info = {
-  name = "My GameJam Game"
-}
